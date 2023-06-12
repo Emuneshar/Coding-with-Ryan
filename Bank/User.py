012e0ceb9ae9069f713528061f5095afd0ba31bb
Eshwar Muneshar <Emuneshar@gmail.com>
2023-06-12 17:58:47 -0400

    created constructor for user

---
+++ Bank/User.py
@@ -0,0 +1,12 @@
+class User:
+
+    def __init__(self, firstName, lastName, age, accountNumber, accountBalance, username, password):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.accountNumber = accountNumber
+        self.accountBalace = accountBalance
+        self.username = username
+        self.password = password
+
+
