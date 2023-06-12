e0e3c538fac0f04d1dd4ae69e20ac53e801edc4d
Eshwar Muneshar <Emuneshar@gmail.com>
2023-06-12 17:45:38 -0400

    created bank menu

--- Bank/Bank.py
+++ Bank/Bank.py
@@ -1,1 +1,10 @@
 print("Welcome to The Coach Bank")
+
+print("You have the following options\n",
+      "1. Open an Account",
+      "2. Withdraw Money",
+      "3. Deposit Money",
+      "4. View account balance",
+      "5. Close Account"
+      )
+
